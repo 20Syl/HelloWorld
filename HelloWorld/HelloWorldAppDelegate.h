aeaccd2e739af0f4de160277016f57a45f789244
20Syl <fillion.sylvain@gmail.com>
2012-11-10 15:41:42 -0500

    Added comment to test github

--- HelloWorld/HelloWorldAppDelegate.h
+++ HelloWorld/HelloWorldAppDelegate.h
@@ -10,6 +10,6 @@
 
 @interface HelloWorldAppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) UIWindow *window; //git test
 
 @end
